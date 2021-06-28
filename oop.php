a71697965e5ec1c96b299b19b1cac9eb867a4f23
Cargalher <cargalher@gmail.com>
2021-06-28 15:58:45 +0200

    second product added

--- oop.php
+++ oop.php
@@ -33,5 +33,13 @@ class Phone extends Product
    }
 }
 // product Instance
-$smartphone = new Product ('Galaxy S21', 'Edge Display, 4K Video Recording, Wireless Charging, Fingerprint Scanner, Multi-SIM',899.99);
+$phone = new Product ('Galaxy S21', 'Edge Display, 4K Video Recording, Wireless Charging, Fingerprint Scanner, Multi-SIM',899.99);
 var_dump($smartphone);
+
+
+
+
+$watch = new Product ('Garmin Forerunner', 'From open water swims to long training rides, this watch is up to the challenge. Its high-performance silicone band and durable Corning® Gorilla® Glass DX lens can handle just about anything.', 600.99);
+var_dump($watch -> name);
+var_dump($watch -> features);
+var_dump($watch -> price);
